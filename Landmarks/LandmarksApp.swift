297ffd36c321c5ba7706bf1108af015e536428b4
xiln7 <xiln7@outlook.com>
2024-01-30 15:17:20 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Zifeng Huang  on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
