ee87e63e0c22ce1d734195198ee07ee1b51351bc
xiln7 <xiln7@outlook.com>
2024-01-31 13:07:06 +0800

    Add CircleImage and MapView

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Turtle Rock, a testament to time's artistry, rises gracefully amidst nature's embrace. Carved by wind and water, its smooth curves and intricate crevices inspire awe. As sunlight dances upon its surface, this majestic formation becomes a living canvas, captivating hearts with its timeless beauty.")
+            }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
